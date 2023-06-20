d594dc33dd3f509783716f041c4caf49a9d0b7bb
Aekangi Patel <aekangi91@gmail.com>
2023-06-20 16:32:13 -0400

    added home page with image, name, and title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
 import Header from "./components/Header"
+import Home from "./components/Home"
+import About from "./components/About"
+import './App.css';
 
+const siteProps = {
+  name: "Aekangi Patel",
+  title: "Software Developer",
+  email: "aekangi82@gmail.com",
+  gitHub: "Aekangi",
+  instagram: "",
+  linkedIn: "aekangipatel",
+  medium: "",
+  twitter: "",
+  youTube: "",
+};
 
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
 const App = () => {
 
   return (
+    <div>
     <Header />
+    <Home name={siteProps.name} title={siteProps.title} />
+    <About />
+    </div>
   )
 }
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import arrowSvg from "../images/down-arrow.svg";
+import PropTypes from "prop-types";
+import image from "../images/portfolioIMG.jpeg";
+
+const imageAltText = "myphoto";
+
+const Home = ({ name, title }) => {
+    return (
+        <section id="home" className="dark">
+            <img className="background" src={image} alt="" />
+            <div style={{ position: "absolute", top: "30%", left: "2rem" }}>
+                <h1>{name}</h1>
+                <h2>{title}</h2>
+            </div>
+            <div style={{ position: "absolute", bottom: "8rem", left: "50%" }}>
+                <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+            </div>
+        </section>
+    );
+}
+Home.defaultProps = {
+    name: "",
+    title: "",
+};
+
+Home.propTypes = {
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+};
+
+export default Home
